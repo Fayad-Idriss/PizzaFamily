32bb578f14a62f495e92228631d016960e7f08d3
Fayad-Idriss BACARI <fayad-idriss.bacari@outlook.com>
2024-10-17 01:32:53 +0200

    first commit

---
+++ PizzaFamilly/Pizza.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+
+
+namespace PizzaFamilly
+{
+
+	public class Pizza
+	{
+		public string nom;
+		public int prix;
+		public bool vegetarien;
+
+
+		public Pizza(string nom, int prix, bool vegetarien)
+		{
+			this.nom = nom;
+			this.prix = prix;
+			this.vegetarien = vegetarien;
+		}
+	}
+}
+
